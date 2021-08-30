5e292df9777823afd202ad17fd9e070737e07d92
Quan Yuan <quan@compoundeye.com>
2021-08-29 23:10:15 -0700

    numpy linspace fix

--- second/utils/eval.py
+++ second/utils/eval.py
@@ -701,7 +701,7 @@ def do_coco_style_eval(gt_annos,
     min_overlaps = np.zeros([10, *overlap_ranges.shape[1:]])
     for i in range(overlap_ranges.shape[1]):
         for j in range(overlap_ranges.shape[2]):
-            min_overlaps[:, i, j] = np.linspace(*overlap_ranges[:, i, j])
+            min_overlaps[:, i, j] = np.linspace(overlap_ranges[:, i, j][0], overlap_ranges[:, i, j][1], int(overlap_ranges[:, i, j][2]))
     mAP_bbox, mAP_bev, mAP_3d, mAP_aos = do_eval_v2(
         gt_annos,
         dt_annos,
